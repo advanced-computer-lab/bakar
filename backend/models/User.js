991195efc82fb21c4942caf94b755f2b5f35a018
berkawy <sberkawy@gmail.com>
2021-11-01 13:59:49 +0200

    Created User model and initialized admin's credentials.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,54 @@
 // TODO: Create User model & create an Administrator entry, refer to issue #1: https://github.com/advanced-computer-lab/bakar/issues/1
+//External variables
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//User's model
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    firstName:{
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type: String,
+        required: true,
+    },
+    homeAddress:{
+        type: String,
+        required: true,
+    },
+    countryCode:{
+        type: Number,
+        required: true,
+    },
+    phone:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    passport:{
+        type: String,
+        required: true,
+    },
+    isAdmin:{
+        type: Boolean,
+    },
+});
+
+module.exports = userSchema;
+
+
+
+
+
