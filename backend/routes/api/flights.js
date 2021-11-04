81913a7b01475595edc2450d872c8d2a7d31ae1d
Rafik-00 <rafikyasser00@gmail.com>
2021-11-04 21:38:50 +0200

    Update flights.js
    added view flight (id:6)
    added update flight (id:7)

--- backend/routes/api/flights.js
+++ backend/routes/api/flights.js
@@ -1,2 +1,23 @@
 // TODO: Specifiy the API schema for flights, refer to issue #3 : https://github.com/advanced-computer-lab/bakar/issues/3
 // also refer to issues #2, #4, #5 and #6.
+const Flight = require('../models/Flight');
+exports.viewFlights = (req, res) => {                                               
+    Flight.find({})
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    };
+    exports.updateFlight = (req,res)=>{
+        Flight.findByIdAndUpdate(req.params.id,req.body).then(result =>{
+    
+            res.status(200).send("flight updated ");
+            console.log('The flight is Updated successfully !');
+        }).catch(err => {
+            console.log(err);
+          });
+    
+      };
+   
