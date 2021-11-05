c6b1062912860db2fa1386b6e0dac0baebd27f07
berkawy <sberkawy@gmail.com>
2021-11-05 17:12:03 +0200

    Created home page and admin can login successfully.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import theme from './Theme';
-import './App.css';
-import { ThemeProvider } from '@emotion/react';
-import { createTheme } from '@mui/material/styles';
-import Flights from './pages/Flights';
+import React from "react";
+import theme from "./Theme";
+import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import Flights from "./pages/Flights";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const generalTheme = createTheme(theme);
 
 function App() {
-	return (
-		<ThemeProvider theme={generalTheme}>
-			<div>
-				<Flights />
-			</div>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={generalTheme}>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/flights" element={<Flights />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
