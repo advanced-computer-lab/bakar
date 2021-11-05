f1b3c24e985da2c274cc5b8ce7c7c1e49771a196
berkawy <sberkawy@gmail.com>
2021-11-05 18:42:36 +0200

    merged viewFlight's commit and tested it but waiting for the flight model to be created.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 const mongoose = require("mongoose");
+
 // Routes
 // require any routes here if needed. e.g: `const books = require('./routes/api/books');`
 const user = require("./routes/api/user.js");
-
+const flights = require("./routes/api/flights.js");
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Server running...'));
+app.get("/", (req, res) => res.send("Server running..."));
 
 // use Routes
 // Use the routes here. e.g: `app.use('/api/books', books);`
--- backend/routes/api/flights.js
+++ backend/routes/api/flights.js
@@ -1,6 +1,6 @@
 // TODO: Specifiy the API schema for flights, refer to issue #3 : https://github.com/advanced-computer-lab/bakar/issues/3
 // also refer to issues #2, #4, #5 and #6.
-const Flight = require('../models/Flight');
+const Flight = require('../../models/Flight');
 exports.viewFlights = (req, res) => {                                               
     Flight.find({})
       .then(result => {
