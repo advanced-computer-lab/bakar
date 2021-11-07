7e3c740dc442256c6e8a2f987bc0b749bfec620e
Mohamed Kamal <darkxnovaz@gmail.com>
2021-11-07 15:16:21 +0200

    Finished functionality of create, update and delete.

--- backend/models/Flight.js
+++ backend/models/Flight.js
@@ -1,53 +1,56 @@
 // TODO: Create Flight model, refer to issue #2 : https://github.com/advanced-computer-lab/bakar/issues/2
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const flightSchema = new Schema({
+const flightSchema = new Schema(
+  {
     flightNo: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     departureTime: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     arrivalTime: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     departureLocation: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     arrivalLocation: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     seatsEcon: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     seatsBus: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     departureTerminal: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     arrivalTerminal: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     priceEcon: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     priceBus: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
-}, { timestamps: true })
-const flight = mongoose.model('flight', flightSchema);
+  },
+  { timestamps: true }
+);
+const flight = mongoose.model("flight", flightSchema);
 module.exports = flight;
--- frontend/src/components/FlightTable/FlightTable.js
+++ frontend/src/components/FlightTable/FlightTable.js
@@ -1,31 +1,36 @@
-import { Paper, Table, TableBody, TableContainer } from '@mui/material';
-import React from 'react';
-import FlightRow from './FlightRow';
+import { Paper, Table, TableBody, TableContainer } from "@mui/material";
+import React from "react";
+import FlightRow from "./FlightRow";
 
 function FlightTable(props) {
-	return (
-		<TableContainer component={Paper}>
-			<Table>
-				<TableBody>
-					{props.flights &&
-						props.flights.map((flight) => (
-							<FlightRow
-								key={flight.flightNo}
-								flightNo={flight.flightNo}
-								departureTime={flight.departureTime}
-								arrivalTime={flight.arrivalTime}
-								departureLocation={flight.departureLocation}
-								arrivalLocation={flight.arrivalLocation}
-								seats={flight.seatsEcon + flight.seatsBus}
-								priceEcon={flight.priceEcon}
-								checks={props.checks}
-								setChecks={props.setChecks}
-							/>
-						))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {props.flights &&
+            props.flights.map((flight) => (
+              <FlightRow
+                key={flight.flightNo}
+                flightNo={flight.flightNo}
+                departureTime={flight.departureTime}
+                arrivalTime={flight.arrivalTime}
+                departureLocation={flight.departureLocation}
+                departureTerminal={flight.departureTerminal}
+                arrivalLocation={flight.arrivalLocation}
+                arrivalTerminal={flight.arrivalTerminal}
+                seatsEcon={flight.seatsEcon}
+                seatsBus={flight.seatsBus}
+                priceEcon={flight.priceEcon}
+                priceBus={flight.priceBus}
+                checks={props.checks}
+                setChecks={props.setChecks}
+                getData={props.getData}
+              />
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default FlightTable;
