293787f9dfb1257dbcef9b4a11d71d4fbd852d60
Mohamed Kamal <darkxnovaz@gmail.com>
2021-11-28 22:10:41 +0200

    Finished Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
-import React from "react";
-import theme from "./Theme";
-import "./App.css";
-import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material/styles";
-import Flights from "./pages/Flights";
-import Login from "./pages/Login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import React from 'react';
+import theme from './Theme';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme, CssBaseline } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Flights from './pages/Flights';
 
 const generalTheme = createTheme(theme);
 
 function App() {
-  return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <ThemeProvider theme={generalTheme}>
-        <Router>
-          <div>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/flights" element={<Flights />} />
-            </Routes>
-          </div>
-        </Router>
-      </ThemeProvider>
-    </LocalizationProvider>
-  );
+	return (
+		<LocalizationProvider dateAdapter={AdapterDateFns}>
+			<ThemeProvider theme={generalTheme}>
+				<CssBaseline />
+				<Router>
+					<div>
+						<Routes>
+							<Route path="/" element={<Homepage />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/flights" element={<Flights />} />
+						</Routes>
+					</div>
+				</Router>
+			</ThemeProvider>
+		</LocalizationProvider>
+	);
 }
 
 export default App;
