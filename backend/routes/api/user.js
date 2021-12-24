48d5be4b45e8b85435650f8587f1270bb93b54a2
Rafik-00 <rafikyasser00@gmail.com>
2021-12-24 13:55:12 +0200

    change password backend

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,40 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const Users = require('../../models/User.js');
-const User = mongoose.model('userSchema', Users);
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-require('dotenv').config();
+const mongoose = require("mongoose");
+const Users = require("../../models/User.js");
+const User = mongoose.model("userSchema", Users);
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+require("dotenv").config();
 const auth = require("../../authorization/authorization");
-
 
 router.post("/login", async (req, res) => {
   passport.authenticate("local", function (err, user, info) {
     if (!user) {
       console.log("Incorrect username or password");
     } else {
-        console.log("user");
-        req.login(user, function (err) {
-          const token = jwt.sign(
-            {
-              username: user.username,
-              firstName: user.firstName,
-              lastName: user.lastName,
-              email: user.email,
-              passport: user.passport,
-              isAdmin: user.isAdmin,
-            },
-            process.env.secretKey,
-            {
-              expiresIn: "24h",
-            }
-          );
-          res.send(token);
-        });
-      }
+      console.log("user");
+      req.login(user, function (err) {
+        const token = jwt.sign(
+          {
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            passport: user.passport,
+            isAdmin: user.isAdmin,
+          },
+          process.env.secretKey,
+          {
+            expiresIn: "24h",
+          }
+        );
+        res.send(token);
+      });
+    }
   })(req, res);
 });
 
@@ -83,55 +82,61 @@ router.post("/register", (req, res) => {
 
 router.put("/", auth, async (req, res) => {
   try {
-    if(!req.user.isAdmin){
+    if (!req.user.isAdmin) {
       console.log(req.user);
-      const updatedUser = await User.updateOne({ username: req.user.username }, req.body).exec();
+      const updatedUser = await User.updateOne(
+        { username: req.user.username },
+        req.body
+      ).exec();
       console.log("The user is Updated successfully !");
       console.log(req.user.username);
-      const updatedToken = jwt.sign( {
-        username: req.user.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email:req.body.email,
-        passport: req.body.passport,
-        isAdmin: false,
-      },process.env.secretKey,
-      {
-        expiresIn: "24h",
-      })
+      const updatedToken = jwt.sign(
+        {
+          username: req.user.username,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          passport: req.body.passport,
+          isAdmin: false,
+        },
+        process.env.secretKey,
+        {
+          expiresIn: "24h",
+        }
+      );
       console.log(updatedUser);
       console.log(updatedToken);
       res.send(updatedToken);
+    } else {
+      console.log("hello from the other side");
+      throw "can't change Admin Status";
     }
-    else{console.log("hello from the other side"); throw "can't change Admin Status";
-  }
   } catch (err) {
     console.log(err);
   }
-
 });
 
 router.put("/password", auth, async (req, res) => {
   try {
-    let user = await User.find({username: req.user.username});
+    let user = await User.findOne({ username: req.user.username }).exec();
     console.log(user);
-    console.log("habd5");
-    console.log("habd5");
-    user.changePassword(req.body.oldpassword, req.body.newpassword, function(err) {
-      if(err){
-        console.log("habd1");
-        res.sendStatus(401);
+    user.changePassword(
+      req.body.oldpassword,
+      req.body.newpassword,
+      function (err) {
+        if (err) {
+          console.log(err);
+          console.log("inccorect old password");
+          res.sendStatus(401);
+        } else {
+          console.log("password changed");
+          res.sendStatus(200);
+        }
       }
-      else{
-        console.log("habd2");
-        res.sendStatus(200);
-      }
-    });
-
+    );
   } catch (error) {
     console.log(error);
     res.sendStatus(401);
-    
   }
 });
 
